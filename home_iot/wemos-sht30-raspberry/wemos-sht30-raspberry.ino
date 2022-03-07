ae5ffdf62e24414a37eee150c6a6b3f892e918a9
Nicolas Uffer <nuffer@MacBook-Air-de-Nicolas.local>
2022-03-07 14:15:56 +0100

    update

--- home_iot/wemos-sht30-raspberry/wemos-sht30-raspberry.ino
+++ home_iot/wemos-sht30-raspberry/wemos-sht30-raspberry.ino
@@ -3,13 +3,16 @@
 #include "SHTSensor.h"
 #include <ESP8266HTTPClient.h>
 
+// note: Do not power with apple charger, deep sleep not working
+
+//#define SERIAL_DEBUG
 
 /* ALL IoT REALEATED */
-String sensor_id = "sht_modlue_1";
+String sensor_id = "sht_modlue_A";
 char* wifi_ssid     = "N&N@Riex";
 char* wifi_password = "664R-FNRR-CTSY-JANA";
 
-unsigned long sleepSeconds = 5*60;
+unsigned long sleepSeconds = 30;//5*60;
 
 SHTSensor sht;
 float humidity = 0.0f;
@@ -18,15 +21,18 @@ float temperature = 0.0f;
 void setup() {
   Wire.begin();
   delay(1000);
-  
+
+  bool init_res = sht.init();
+
+  #ifdef SERIAL_DEBUG
   Serial.begin(115200);
   delay(1000); // let serial console settle
-
-  if (sht.init()) {
+  if (init_res) {
       Serial.print("init(): success\n");
   } else {
       Serial.print("init(): failed\n");
   }
+  #endif
 
   // only supported by SHT3x
   sht.setAccuracy(SHTSensor::SHT_ACCURACY_MEDIUM); 
@@ -53,14 +59,15 @@ void sendData()
    http.addHeader("Content-Type", "application/json");
   
    String data = "{\"sensor_id\":\"" + sensor_id + "\",\"temperature\":"+ String(temperature) + ",\"humidity\":"+ String(humidity) + "}";
-   Serial.print("POST:");
-   Serial.println(data);
    int httpCode = http.POST(data);
    String payload = http.getString(); //Get the response payload
- 
+
+   #ifdef SERIAL_DEBUG
+   Serial.print("POST:");
+   Serial.println(data);
    Serial.println(httpCode);   //Print HTTP return code
    Serial.println(payload);    //Print request response payload
- 
+   #endif
    http.end();  //Close connection
 }
 
@@ -70,15 +77,25 @@ void read_sensor()
     {
         humidity = sht.getHumidity();
         temperature = sht.getTemperature();
+        #ifdef SERIAL_DEBUG
         Serial.print("H: ");
         Serial.print(humidity, 2);
         Serial.print(" T:  ");
         Serial.println(temperature, 2);
+        #endif
     }
     else
     {
+       #ifdef SERIAL_DEBUG
        Serial.print("Error in readSample()\n");
+       #endif
     }
 }
 
-void loop() {}
+void loop() {
+  /*
+  read_sensor();
+  sendData();
+  delay(10000);
+  */
+  }
