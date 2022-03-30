5408512b48a65b33887cbad5a2115c6295742963
Nicolas Uffer <nicolas.uffer@gmail.com>
2022-03-30 09:52:57 +0200

    Update

--- projects/humidifier/humidifier.ino
+++ projects/humidifier/humidifier.ino
@@ -1,17 +1,21 @@
 #include <Wire.h>
+#include <EEPROM.h>
 #include "SHTSensor.h"
 
 #define PIN_FAN 6
 #define PIN_HUMIDIFIER 7
 
+//EEPROM address to start reading from
+int eeAddressHigh = 0; 
+int eeAddressLow = 4; 
 
 SHTSensor sht;
 boolean activate = false;
 float humidity = 0.0f;
 float temperature = 0.0f;
 
-float humidity_low = 90.0f;
-float humidity_high = 95.0f;
+float humidity_low = 95.0f;
+float humidity_high = 100.0f;
 
 
 void setup() {
@@ -21,6 +25,16 @@ void setup() {
   
   Serial.begin(115200);
   delay(1000);
+  
+  //get the data from memory
+  EEPROM.get(eeAddressHigh,humidity_high);
+  EEPROM.get(eeAddressLow,humidity_low);
+
+  Serial.print("Loaded HIGH humidity from memory:");
+  Serial.println(humidity_high);
+
+  Serial.print("Loaded LOW humidity from memory:");
+  Serial.println(humidity_low);
 
   if (sht.init()) {
       Serial.print("init(): success\n");
@@ -60,6 +74,28 @@ void read_sensor()
 }
 
 void loop() {
+
+   if (Serial.available() > 0) {
+      String data = Serial.readString();
+      /* close serial connection */
+      if( data.substring(0,1) == "H" )
+      {          
+        humidity_high = data.substring(1).toFloat();
+        EEPROM.put(eeAddressHigh,humidity_high);
+        Serial.print("Set HIGH value of humidity at: ");   
+        Serial.println(humidity_high);
+      }
+      if( data.substring(0,1) == "L" )
+      {          
+        humidity_high = data.substring(1).toFloat();
+        EEPROM.put(eeAddressLow,humidity_high);
+        Serial.print("Set LOW value of humidity at: ");   
+        Serial.println(humidity_high);
+      }
+
+    }
+
+  
   read_sensor();
 
   if(humidity < humidity_low){
